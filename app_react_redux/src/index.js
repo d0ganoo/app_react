d2ce9f2433d537e36c3fc8f8e4dc8e96bb3cef6f
d0ganoo <faucheriefrancois@gmail.com>
2019-05-14 13:09:46 +0000

    Ajout de la config pour redux devtools

--- app_react_redux/src/index.js
+++ app_react_redux/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import configureStore from './store';
-import './css/index.css';
+import './assets/css/index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 
